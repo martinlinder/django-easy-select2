784e6448579792ab6a130a261408c0ad72b45e4a
LinderM <martin.linder@xal.com>
2021-12-17 18:53:39 +0100

    changed force_text and ugettext_lazy to be compatible with django 4.0

--- easy_select2/forms.py
+++ easy_select2/forms.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
 from django import forms
-from django.utils.encoding import force_text
-from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_str
+from django.utils.translation import gettext_lazy as _
 
 M = _('Hold down "Control", or "Command" on a Mac, to select more than one.')
 
